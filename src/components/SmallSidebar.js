6d40f2108f6d6196ff55aff978688ff4931b4aa2
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-18 13:17:49 +0200

    solve a warning

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,5 +1,7 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import SidebarLinks from './SidebarLinks';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './logo';
 
 export default function SmallSidebar(params) {
   return (
@@ -7,24 +9,10 @@ export default function SmallSidebar(params) {
       <div className="sidebar-container">
         <div className="content">
           <button className="close-btn">
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 352 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
-            </svg>
+            <FaTimes />
           </button>
           <header>
-            <img
-              src="/static/media/logo.810e48648353f298a3ea441b8ca95f4f.svg"
-              alt="jobster logo"
-              className="logo"
-            />
+            <Logo />
           </header>
           <SidebarLinks />
         </div>
