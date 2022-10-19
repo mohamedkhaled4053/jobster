d1a31ce982453fcf9f53b686b66c10d1417be4a6
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-19 17:02:37 +0200

    handle change in Profile

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,20 +1,56 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { FormRow } from '../../components';
 
 export default function Profile() {
-  let { name, email, lastName, location } = useSelector((store) => store.user.user);
+  let user = useSelector((store) => store.user.user);
 
+  let [userData, setUserData] = useState({
+    name: user.name,
+    lastName: user.lastName,
+    email: user.email,
+    location: user.location,
+  });
+
+  function handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  }
+
+  let { name, lastName, email, location } = userData;
   return (
     <Wrapper>
-      <form class="form">
+      <form className="form">
         <h3>profile</h3>
-        <div class="form-center">
-          <FormRow name='name' type='text' value={name}/>
-          <FormRow name='lastName' type='text' value={lastName} labelText='last name'/>
-          <FormRow name='email' type='email' value={email}/>
-          <FormRow name='location' type='text' value={location}/>
-          <button type="submit" class="btn btn-block">
+        <div className="form-center">
+          <FormRow
+            name="name"
+            type="text"
+            value={name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            name="lastName"
+            type="text"
+            value={lastName}
+            labelText="last name"
+            handleChange={handleChange}
+          />
+          <FormRow
+            name="email"
+            type="email"
+            value={email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            name="location"
+            type="text"
+            value={location}
+            handleChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block">
             save changes
           </button>
         </div>
