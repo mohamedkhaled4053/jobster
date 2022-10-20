cff98b4030c5eb33b394dd6c04f5d10b16a99eb3
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-20 08:48:24 +0200

    define state in add-job page

--- src/components/FormRowSelect.js
+++ src/components/FormRowSelect.js
@@ -1,13 +1,12 @@
-export default function FormRowSelect({ name, value, labelText, handleChange }) {
+export default function FormRowSelect({ name, value, labelText, handleChange, list }) {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
         {labelText || name}
       </label>
       <select name={name} id={name} className="form-select" value={value}>
-        <option value="interview">interview</option>
-        <option value="declined">declined</option>
-        <option value="pending">pending</option>
+        {list.map(option=>
+          <option value={option}>{option}</option>)}
       </select>
     </div>
   );
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,23 +1,44 @@
+import { useState } from 'react';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { FormRow } from '../../components';
 import FormRowSelect from '../../components/FormRowSelect';
 
+let initialState = {
+  position: '',
+  company: '',
+  jobLocation: '',
+  status: 'pending',
+  jobType: 'full-time',
+};
+
 export default function AddJob() {
+  // define the state and destructure it to type less
+  let [jobData, setJobData] = useState(initialState);
+  let { position, company, jobLocation, status, jobType } = jobData;
+  // define variables
+  let jobTypesList = ['full-time', 'part-time', 'remote', 'internship'];
+  let statusList = ['pending', 'interview', 'declined'];
+
   return (
     <Wrapper>
       <form className="form">
         <h3>add job</h3>
         <div className="form-center">
-          <FormRow name="position" type="text" value="placeholder" />
-          <FormRow name="company" type="text" value="placeholder" />
+          <FormRow name="position" type="text" value={position} />
+          <FormRow name="company" type="text" value={company} />
           <FormRow
-            name="position"
+            name="jobLoction"
             type="text"
-            value="placeholder"
+            value={jobLocation}
             labelText="job location"
           />
-          <FormRowSelect name='status' value='love'/>
-          <FormRowSelect name='jobType' value='love' labelText='job type'/>
+          <FormRowSelect name="status" value={status} list={statusList} />
+          <FormRowSelect
+            name="jobType"
+            value={jobType}
+            labelText="job type"
+            list={jobTypesList}
+          />
           <div className="btn-container">
             <button type="button" className="btn btn-block clear-btn">
               clear
