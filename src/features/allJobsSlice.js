83e4c56cc4f7d84c89c719b2a87f1b5e98e8b058
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-23 20:18:05 +0200

    get all jobs data

--- src/features/allJobsSlice.js
+++ src/features/allJobsSlice.js
@@ -1,12 +1,46 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import authHeader from '../utils/authHeader';
+import { customFetch } from '../utils/axios';
+import checkForUnauthorized from '../utils/checkForUnauthorized';
 
-let initialState= {
-    jobs : []
-}
+let initialState = {
+  isLoading: false,
+  jobs: [],
+  numOfPages: 0,
+  totalJobs: 0,
+};
+
+export let getAllJobs = createAsyncThunk('getAllJobs', async (_, thunkAPI) => {
+  let url = '/jobs';
+  try {
+    let res = await customFetch.get(url, authHeader(thunkAPI));
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error.response);
+    return checkForUnauthorized(error, thunkAPI);
+  }
+});
 
 let AlljobsSlice = createSlice({
-    name:'allJobs',
-    initialState
-})
+  name: 'allJobs',
+  initialState,
+  extraReducers: {
+    [getAllJobs.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getAllJobs.fulfilled]: (
+      state,
+      { payload: { jobs, numOfPages, totalJobs } }
+    ) => {
+      return { ...state, isLoading: false, jobs, numOfPages, totalJobs };
+    },
+    [getAllJobs.rejected]: (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+    },
+  },
+});
 
-export default AlljobsSlice.reducer
+export default AlljobsSlice.reducer;
