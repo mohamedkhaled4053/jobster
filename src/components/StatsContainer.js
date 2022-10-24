8c237bd065c623198b9abface0a9d374d13207c7
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-24 18:22:21 +0200

    display stats dynamically

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import Wrapper from '../assets/wrappers/StatsContainer'
+import { useDispatch, useSelector } from 'react-redux';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import { getStats } from '../features/statsSlice';
 import StatItem from './StatItem';
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
 
 export default function StatsContainer() {
-    let colores = ["#e9b949","#647acb","#d66a6a"]
+  // get data from the store
+  let { pending, interview, declined } = useSelector((store) => store.stats);
+  // prepare stats data to iterate over it
+  let statsData = [
+    {
+      color: '#e9b949',
+      iconBackGround:'#fcefc7',
+      icon: <FaSuitcaseRolling />,
+      data: pending,
+      text: 'Pending Applications ',
+    },
+    {
+      color: '#647acb',
+      iconBackGround:'#e0e8f9',
+      icon: <FaCalendarCheck />,
+      data: interview,
+      text: 'Interviews Scheduled',
+    },
+    {
+      color: '#d66a6a',
+      iconBackGround:'#ffeeee',
+      icon: <FaBug />,
+      data: declined,
+      text: 'Jobs Declined',
+    },
+  ];
+
+  // get stats on first render
+  let dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getStats());
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <Wrapper>
-        <StatItem color={colores[0]}/>
-        <StatItem color={colores[1]}/>
-        <StatItem color={colores[2]}/>
-
+      {statsData.map((stat, index) => (
+        <StatItem key={index} {...stat} />
+      ))}
     </Wrapper>
   );
 }
--- src/features/statsSlice.js
+++ src/features/statsSlice.js
@@ -1,14 +1,32 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import authHeader from '../utils/authHeader';
+import { customFetch } from '../utils/axios';
+import checkForUnauthorized from '../utils/checkForUnauthorized';
 
 let initialState = {
-    
-}
+  pending: 0,
+  interview: 0,
+  declined: 0,
+};
 
-export const getStats = createAsyncThunk('getStats',()=>{
+export const getStats = createAsyncThunk('getStats', async (_, thunkApi) => {
+  try {
+    let res = await customFetch('/jobs/stats', authHeader(thunkApi));
+    return res.data;
+  } catch (error) {
+    return checkForUnauthorized();
+  }
+});
 
-})
+const statsSlice = createSlice({
+  name: 'stats',
+  initialState,
+  extraReducers: {
+    [getStats.fulfilled]: (state, { payload }) => {
+      let { defaultStats, monthlyApplications } = payload;
+      return { ...state, ...defaultStats };
+    },
+  },
+});
 
-const statsSlice = createSlice({ name: 'stats', initialState });
-
-
-export default statsSlice.reducer
+export default statsSlice.reducer;
