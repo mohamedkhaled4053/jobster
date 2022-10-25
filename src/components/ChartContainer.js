11f630c5d495447e504c48b275b3696450e13514
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-25 11:15:18 +0200

    minor edit in ChartContainer.js

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -5,17 +5,17 @@ import AreaChartComponent from './AreaChartComponent';
 import BarChartComponent from './BarChartComponent';
 
 export default function ChartContainer() {
+  // local state
   let [isBarChart, setIsBarChart] = useState(true);
+  // get chart data from store
   let { chartData } = useSelector((store) => store.stats);
   return (
     <Wrapper>
       <h4>Monthly Applications</h4>
-      <button type="button" onClick={() => setIsBarChart(true)}>
-        Bar Chart
+      <button type="button" onClick={() => setIsBarChart(!isBarChart)}>
+        switch to {isBarChart ? 'Area Chart' : 'Bar Chart'}
       </button>
-      <button type="button" onClick={() => setIsBarChart(false)}>
-        Area Chart
-      </button>
+
       {isBarChart ? (
         <BarChartComponent data={chartData} />
       ) : (
